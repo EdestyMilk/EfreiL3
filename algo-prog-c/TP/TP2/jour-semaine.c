cbc6b7f1a3fa59c8e943e2b703362cd4fb698961
SamSepi0l <flynn@lives.com>
2022-09-06 16:27:56 +0200

    [TP02][Exo4] Jours de la semaine // MODIF

--- algo-prog-c/TP/TP2/jour-semaine.c
+++ algo-prog-c/TP/TP2/jour-semaine.c
@@ -1,32 +1,11 @@
 #include <stdio.h>
 int main(void){
     int entry;
+    char const *jours[7]={"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"};
     printf("Entrez le num d'un jour de la semaine:");
     scanf("%d", &entry);
     if(entry <= 7 && entry != 0){
-        switch(entry){
-            case 1:
-                printf("On est Lundi\n");
-                break;
-            case 2:
-                printf("On est Mardi\n");
-                break;
-            case 3:
-                printf("On est Mercredi\n");
-                break;
-            case 4:
-                printf("On est Jeudi\n");
-                break;
-            case 5:
-                printf("On est Vendredi\n");
-                break;
-            case 6:
-                printf("On est samedi\n");
-                break;
-            case 7:
-                printf("On est dimanche\n");
-                break;
-        }
+        printf("On est %s\n", jours[entry-1]);
     }
     else{
         printf("Numéro invalide\n");
