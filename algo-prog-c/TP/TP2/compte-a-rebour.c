c36a696c2a090996f0f5ea2509b84b179e356a8c
SamSepi0l <flynn@lives.com>
2022-09-06 14:37:29 +0200

    [TP02][Exo2] Compte à rebours

---
+++ algo-prog-c/TP/TP2/compte-a-rebour.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (void){
+    int entry;
+    printf("Entrez un nombre : ");
+    scanf("%d",&entry);
+    while(entry > 0){
+        printf("%d\n",entry);
+        entry--;
+    }
+    printf("Arouf Gangsra takpt\n ");
+}
