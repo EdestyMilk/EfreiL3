869da3638dd854e6ad92f486338d332d8d2339f1
SamSepi0l <flynn@lives.com>
2022-09-06 16:20:09 +0200

    [TP02][Exo8] Conversion de date // MODIF

--- algo-prog-c/TP/TP2/conversion-date.c
+++ algo-prog-c/TP/TP2/conversion-date.c
@@ -1,43 +1,7 @@
 #include <stdio.h>
 int main(void){
     int day,month,year;
+    char const *mois[12]= {"Janiver", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"};
     scanf("%d/%d/%d",&day,&month,&year);
-    switch(month){
-        case 1:
-            printf("%d Janvier %d\n", day, year);
-            break;
-        case 2:
-            printf("%d Février %d\n", day, year);
-            break;
-        case 3:
-            printf("%d Mars %d\n", day, year);
-            break;
-        case 4:
-            printf("%d Avril %d\n", day, year);
-            break;
-        case 5:
-            printf("%d Mai %d\n", day, year);
-            break;
-        case 6:
-            printf("%d Juin %d\n", day, year);
-            break;
-        case 7:
-            printf("%d Juillet %d\n", day, year);
-            break;
-        case 8:
-            printf("%d Août %d\n", day, year);
-            break;
-        case 9:
-            printf("%d Septembre %d\n", day, year);
-            break;
-        case 10:
-            printf("%d Octobre %d\n", day, year);
-            break;
-        case 11:
-            printf("%d Novembre %d\n", day, year);
-            break;
-        case 12:
-            printf("%d Décembre %d\n", day, year);
-            break;
-    }
+    printf("%d %s %d", day, mois[month-1], year);
 }
