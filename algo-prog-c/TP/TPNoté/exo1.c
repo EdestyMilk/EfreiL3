14b3e8bd493731e615197b3624d52d702ac82e48
Kevin Flynn <flynn@pop-os.localdomain>
2022-10-17 11:22:50 +0200

    Finition et commentaire

--- algo-prog-c/TP/TPNoté/exo1.c
+++ algo-prog-c/TP/TPNoté/exo1.c
@@ -5,7 +5,7 @@ const int size1=15;
 const int size2=7;
 int tab[15][7];
 int tabOccur[9]={0,0,0,0,0,0,0,0,0};
-void nb_occurences(){
+void nb_occurences(){ // Fonction permettant de récupérer le nombre d'occurences
     for(int a=0;a!=size1;a++){
         for(int b=0;b != size2; b++){
             tabOccur[(tab[b][a]-1)]++;
@@ -13,7 +13,7 @@ void nb_occurences(){
     }
 }
 
-void desc_tri(){
+void desc_tri(){ // Fonction triant le tableau en descendant
     int tmp;
     for(int a=0;a != size1;a++){
         for(int b=size2; b!=0;b--){
@@ -27,7 +27,7 @@ void desc_tri(){
         }
     }
 }
-void del_line(){
+void del_line(){ // Fonction supprimant les nombres ayant des occurences inférieur ou égale à 3
         for(int c=0;c != 9;c++){
             if(tabOccur[c] <= 3){
                 for(int d=0;d != size1; d++){
@@ -42,7 +42,7 @@ void del_line(){
 
 int main(void){
     srand(time(NULL));
-    for(int a=0;a!=size1;a++){
+    for(int a=0;a!=size1;a++){ // Permet de générer un tableau aléatoire entre 1 et 9
         for(int b=0; b != size2; b++){
             tab[a][b] = rand() % (9 - 1 + 1) + 1;
             printf("%d ",tab[a][b]);
--- algo-prog-c/TP/TPNoté/exo2.c
+++ algo-prog-c/TP/TPNoté/exo2.c
@@ -8,29 +8,29 @@ int main(void){
     int sum_diag[n];
     int is_perfect=0;
 
-    printf("Donnez la taille n pour créer la matrice n*n: ");
+    printf("Donnez la taille n pour créer la matrice n*n: "); // Création et allocation mémoire du tableau
     scanf("%d", &n);
     int tab[n][n];
 
-    for(int a=0;a != n; a++){
+    for(int a=0;a != n; a++){ // Remplissage du tableau
         for(int b=0; b!= n; b++){
             int tmp;
             printf("Entrez la valeur pour %d*%d: ",a+1, b+1);
             scanf("%d", &tmp);
-            if(tmp > n*n && tmp > 1)
+            if(tmp > n*n && tmp > 1) // Permet de savoir si le carrée est parfait
                 is_perfect=1;
             tab[a][b]=tmp;
         }
     }
     
-    for(int a=0;a != n; a++){
+    for(int a=0;a != n; a++){ // Calcul de la somme des lignes
         for(int b=0; b!= n; b++){
             int tmp=tab[a][b];
             sum_line=tmp+sum_line;
         }
     }
 
-    for(int c=0; c != n ; c++){
+    for(int c=0; c != n ; c++){ // Calcul de la somme des colonnes
         for(int a=0;a != n; a++){
             sum_col=(tab[a][c]+sum_col);
         }
@@ -39,7 +39,7 @@ int main(void){
     sum_diag[0]=0;
     sum_diag[1]=0;
 
-    for(int d=0; d != n;d++){
+    for(int d=0; d != n;d++){ // Calcul de la somme des diagonales
         
         sum_diag[0]=sum_diag[0]+tab[d][d];
     }
@@ -51,11 +51,9 @@ int main(void){
     printf("Somme Colonne : %d\n Somme Ligne : %d\n", sum_col, sum_line);
     printf("Somme diag 1: %d Somme diag 2: %d\n", sum_diag[0], sum_diag[1]);
 
-    if(sum_col == sum_line){
-        if(sum_col/n+sum_line/n == sum_diag[0]+sum_diag[1]){
-            printf("La matrice est un carré magique ");
-        }
-        if(is_perfect == 0)
+    if(sum_col == sum_line sum_col/n+sum_line/n == sum_diag[0]+sum_diag[1]){ // Condition pour vérifier si le carrée est magique
+        printf("La matrice est un carré magique ");
+        if(is_perfect == 0) // Condition pour savoir si il est parfait
             printf("parfait\n");
         else
             printf("\n");
